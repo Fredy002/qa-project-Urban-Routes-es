df5f7ae6c3f0c334461dc0cd00ea8825eb512859
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-10-15 12:06:42 -0500

    update url

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://029f5e89-1ed2-457a-8c61-c3ff8b9e57c2.serverhub.tripleten-services.com'
+urban_routes_url = 'https://cnt-f6c2ac11-334e-4447-baf4-665162b1a985.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
